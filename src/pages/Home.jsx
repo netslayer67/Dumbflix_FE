001f4106cf755fa988e82d3d504da981d1c5156b
sayedjilliyan <jilliyan67@gmail.com>
2022-09-27 09:13:21 +0700

    o

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import MovieContainer from "../components/MovieContainer";
 import SeriesContainer from "../components/SeriesContainer";
 
-
 function Home() {
-  
   return (
     <>
       <div>
         <div className="background-hero-home container-fluid mx-auto">
           <div className="hero ms-5 text-light d-flex flex-column align-content-center justify-content-center">
-            <h1 className="text-size text-start">PEAKY BLINDERS</h1>
+            <h1 className="text-size text-start">Money Heist</h1>
             <p className="">
-              Peaky Blinders is an epic centred on a crime family of mixed Irish
-              Catholic and Romani origins based in Birmingham, England, starting
-              in 1919, several months after the end of the First World War in
-              November 1918.
+              This original Netflix series, originally titled La Casa de Papel,
+              follows the Professor (Álvaro Morte), a criminal mastermind who
+              brings eight thieves together to take hostages.
             </p>
             <div className="mb-4">
               <span>2013</span>
@@ -31,8 +28,8 @@ function Home() {
           </div>
         </div>
       </div>
-        <MovieContainer/>
-        <SeriesContainer/>
+      <SeriesContainer />
+      <MovieContainer />
     </>
   );
 }
